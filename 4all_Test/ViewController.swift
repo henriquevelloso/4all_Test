4e7b9219734943e1f46c98bba16159190bf29b55
Henrique Velloso <henriquevelloso@gmail.com>
2019-03-09 10:26:59 -0300

    Initial Commit

---
+++ 4all_Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  4all_Test
+//
+//  Created by Henrique Velloso on 09/03/19.
+//  Copyright © 2019 Henrique Velloso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
